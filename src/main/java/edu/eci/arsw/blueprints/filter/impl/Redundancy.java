21f8e6b28aab0e2cddb1b9c6bed49a5f23440278
JVARGAS <jaider.vargas-n@mail.escuelaing.edu.co>
2025-03-04 16:29:31 -0500

    importar clases

---
+++ src/main/java/edu/eci/arsw/blueprints/filter/impl/Redundancy.java
@@ -0,0 +1,4 @@
+package edu.eci.arsw.blueprints.filter.impl;
+
+public class Redundancy {
+}
---
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/InMemoryBlueprintPersistence.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.persistence.impl;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+
+import java.util.*;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class InMemoryBlueprintPersistence implements BlueprintsPersistence{
+
+    private final Map<Tuple<String,String>,Blueprint> blueprints=new HashMap<>();
+
+    public InMemoryBlueprintPersistence() {
+        //load stub data
+        Point[] pts=new Point[]{new Point(140, 140),new Point(115, 115)};
+        Blueprint bp=new Blueprint("_authorname_", "_bpname_ ",pts);
+        blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+
+    }
+
+    @Override
+    public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
+        if (blueprints.containsKey(new Tuple<>(bp.getAuthor(),bp.getName()))){
+            throw new BlueprintPersistenceException("The given blueprint already exists: "+bp);
+        }
+        else{
+            blueprints.put(new Tuple<>(bp.getAuthor(),bp.getName()), bp);
+        }
+    }
+
+    @Override
+    public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
+        Blueprint bp = blueprints.get(new Tuple<>(author, bprintname));
+        if(bp==null) throw new BlueprintNotFoundException("Not results found");
+        return bp ;
+    }
+
+    @Override
+    public Set<Blueprint> getBlueprintsByAuthor(String author) throws BlueprintNotFoundException{
+        Set<Blueprint> bpps = new HashSet<>();
+        for (Map.Entry<Tuple<String,String>, Blueprint> entry : blueprints.entrySet()) {
+            if (entry.getKey().getElem1().equals(author)) {
+                bpps.add(entry.getValue());
+            }
+        }
+        if(bpps.isEmpty()) throw new BlueprintNotFoundException("Not results found");
+        return bpps;
+    }
+    @Override
+    public Set<Blueprint> getAllBluePrints() throws BlueprintNotFoundException{
+        if(blueprints.isEmpty()) throw new BlueprintNotFoundException("Not results found");
+        return (Set<Blueprint>) blueprints.values();
+    }
+
+
+
+}
